16825ab9831d81eee0fb82c36cb7a696a33148f3
Michael Rash <mbr@cipherdyne.org>
2016-06-03 23:07:18 -0400

    refactor --cover-corpus support

--- test/afl/afl-cov-generator-live.sh
+++ test/afl/afl-cov-generator-live.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/sh -x
 
 CODE_DIR=./fwknop-codecov.git
 AFL_COV=../afl-cov
 AFL_TEST_CASES=./fwknop-afl.git/test/afl/fuzzing-output/server-access.out
 
-$AFL_COV --live --sleep 2 --afl-queue-id-limit 3 -d $AFL_TEST_CASES --coverage-cmd "LD_LIBRARY_PATH=$CODE_DIR/lib/.libs $CODE_DIR/server/.libs/fwknopd -c $CODE_DIR/test/conf/ipt_snat_fwknopd.conf -a AFL_FILE -O $CODE_DIR/test/conf/override_no_digest_tracking_fwknopd.conf -f -t --exit-parse-config -v -v -v -r `pwd`" --code-dir $CODE_DIR $@ > /dev/null
+$AFL_COV --live --sleep 2 --afl-queue-id-limit 3 -d $AFL_TEST_CASES --coverage-cmd "LD_LIBRARY_PATH=$CODE_DIR/lib/.libs $CODE_DIR/server/.libs/fwknopd -c $CODE_DIR/test/conf/ipt_snat_fwknopd.conf -a AFL_FILE -O $CODE_DIR/test/conf/override_no_digest_tracking_fwknopd.conf -f -t --exit-parse-config -v -v -v -r `pwd`" --code-dir $CODE_DIR $@
 
 exit 0
