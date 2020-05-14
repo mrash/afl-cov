89801bb525e79d769838ad5cddd774c2b066d774
van Hauser <vh@thc.org>
2020-05-14 10:17:34 +0200

    display time as clock too

--- afl-stat.sh
+++ afl-stat.sh
@@ -9,7 +9,12 @@ while [ -n "$1" ]; do
   test -d "$1" || { echo Error: not a directory: $1 ;  } 
   test -e "$1"/fuzzer_stats || { echo Error: not an afl-fuzz -o out directory ;  }
   echo File: `realpath "$1"`
-  egrep 'run_time|execs_done|execs_per_sec|paths_total|^unique_|stability' "$1"/fuzzer_stats | sort | tee -a "$1"/stats.out
+  {
+    egrep 'run_time|execs_done|execs_per_sec|paths_total|^unique_|stability' "$1"/fuzzer_stats
+    SECONDS=`egrep run_time "$1"/fuzzer_stats | awk '{print$3}'`
+    TIME=`date -u -d "@$SECONDS" +"%T"`
+    echo "run_clock         : $TIME"
+  } | sort | tee -a "$1"/stats.out
   LINES=
   test -e "$1"/cov/afl-cov.log && LINES=`grep -w lines "$1"/cov/afl-cov.log|tail -n 1|sed 's/.*(//'|sed 's/ .*//'`
   echo "coverage          : $LINES" | tee -a "$1"/stats.out
