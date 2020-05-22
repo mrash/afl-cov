71ce66cce7f1decbcd8aa6870afcaecdcf4776a5
van Hauser <vh@thc.org>
2020-05-22 21:38:19 +0200

    fix script

--- afl-cov.sh
+++ afl-cov.sh
@@ -9,12 +9,22 @@ test "$1" = "-h" -o -z "$1" && {
   echo Example: $0 ../target/out \"tools/target @@\"
   exit 1
 }
+
 test -d "$1" || { echo Error: not a directory: $1 ; exit 1 ; } 
 test -e "$1"/queue || { echo Error: not an afl-fuzz -o out directory ; exit 1 ; }
+
+HOMEPATH=`dirname $0`
 DST=`realpath "$1"`
+export PATH=$HOMEPATH:$PATH
+
 afl-cov -v -d "$DST" --cover-corpus --coverage-cmd "$2" --code-dir . --overwrite
+
 test -e "$1"/fuzzer_stats && {
   echo "runtime           :" $(expr `grep last_update "$DST"/fuzzer_stats|awk '{print$3}'` - `grep start_time "$DST"/fuzzer_stats|awk '{print$3}'`) seconds
   egrep 'execs_done|paths_total|^unique_|stability' "$DST"/fuzzer_stats
-}
+  LINES=
+  test -e "$1"/cov/afl-cov.log && LINES=`grep -w lines "$1"/cov/afl-cov.log|tail -n 1|sed 's/.*(//'|sed 's/ .*//'`
+  echo "coverage          : $LINES"
+} | tee "$1"/stats.out
+
 echo open "file://$DST/cov/web/index.html"
