9b9d121b5c6e5155a0315d089203ab50d262dfc0
van Hauser <vh@thc.org>
2020-05-23 14:37:59 +0200

    -v option

--- afl-cov.sh
+++ afl-cov.sh
@@ -3,9 +3,12 @@
 # easy wrapper script for afl-cov
 #
 test "$1" = "-h" -o -z "$1" && { 
-  echo Syntax: $0 out-dir \"exec cmd --foo @@\"
+  echo "Syntax: $0 [-v] out-dir \"exec cmd --foo @@\""
+  echo
   echo Generates the coverage information for an AFL run.
   echo Must be run from the top directory of the coverage build.
+  echo The -v option enables verbose output
+  echo
   echo Example: $0 ../target/out \"tools/target @@\"
   exit 1
 }
@@ -17,7 +20,8 @@ HOMEPATH=`dirname $0`
 DST=`realpath "$1"`
 export PATH=$HOMEPATH:$PATH
 
-afl-cov -v -d "$DST" --cover-corpus --coverage-cmd "$2" --code-dir . --overwrite
+test "$1" = "-v" && { OPT="-v" ; shift ; }
+afl-cov $OPT -d "$DST" --cover-corpus --coverage-cmd "$2" --code-dir . --overwrite
 
 test -e "$1"/fuzzer_stats && {
   echo "runtime           :" $(expr `grep last_update "$DST"/fuzzer_stats|awk '{print$3}'` - `grep start_time "$DST"/fuzzer_stats|awk '{print$3}'`) seconds
