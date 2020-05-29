fd7ba2bdc824a3368dac1b7e3f53d3756c6ca7dd
van Hauser <vh@thc.org>
2020-05-29 22:32:45 +0200

    small change to build script

--- afl-cov-build.sh
+++ afl-cov-build.sh
@@ -3,6 +3,7 @@
 test -z "$1" -o "$1" = "-h" && {
   echo "Syntax: $0 <command> [options]"
   echo Sets build options for coverage instrumentation with gcov/lcov.
+  echo Set CC/CXX environment variables if you do not want gcc/g++.
   echo Example: "$0 ./configure --disable-shared"
 }
 
@@ -10,6 +11,7 @@ test -z "$CC" && export CC=gcc
 test -z "$CXX" && export CXX=g++
 export CFLAGS="-fprofile-arcs -ftest-coverage"
 export CXXFLAGS="$CFLAGS"
+export CPPFLAGS="$CFLAGS"
 export LDFLAGS="-lgcov --coverage"
 
 $*
